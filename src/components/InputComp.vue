cdfed1d98bddf57bd6e797fa4a14eaf435ef0bb7
tya6ivue <nishant_tyagi@excellencetechnologies.in>
2021-08-11 18:01:49 +0530

    added store and other functionality

--- src/components/InputComp.vue
+++ src/components/InputComp.vue
@@ -4,24 +4,17 @@
     <v-app id="inspire">
       <v-main>
         <v-container>
-    <v-btn class="ma-16" depressed color="error" @click="addAnimal"> Create </v-btn>
-          <v-row>
-            <v-col
-              v-for="(animal, index) in categories"
-              :key="index"
-              cols="4"
-            >
-              <v-card >
+          <AddAnimal></AddAnimal>
 
-                <v-img
-                 :src="animal.imgSrc"
-                 class="width:50px"
-                  
-                >
+          <p class="font-italic ma-10">Animals of the kingdom</p>
+          <v-row>
+            <v-col v-for="(animal, index) in categories" :key="index" cols="4">
+              <v-card>
+                <v-img :src="animal.imgSrc" class="width:50px">
                   <v-btn class="float-right" @click="deleteAnimal"> X </v-btn>
                 </v-img>
                 <v-card-title>
-            {{animal.name}}
+                  {{ animal.name }}
                 </v-card-title>
               </v-card>
             </v-col>
@@ -33,16 +26,29 @@
 </template>
 
 <script>
+import AddAnimal from "./AddAnimal.vue";
 import { mapActions, mapState } from "vuex";
 export default {
   name: "inputComp",
-  computed: {
-    ...mapState('Anima', ["categories"]),
+  components: {
+    AddAnimal,
   },
 
-methods: {
-  ...mapActions('')
-}
+  computed: {
+    ...mapState("Anima", ["categories"]),
+  },
 
+  methods: {
+    ...mapActions("Anima", ["AddItems"]),
+    ...mapActions("Anima", ["DeleteItems"]),
+
+    addAnimal() {
+      this.AddItems({});
+    },
+    deleteAnimal() {
+      alert("srgtrh");
+      this.DeleteItems({});
+    },
+  },
 };
 </script>
