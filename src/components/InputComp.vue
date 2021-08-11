859cc1cf777df317d79623ceb1c7afc7cc205657
tya6ivue <nishant_tyagi@excellencetechnologies.in>
2021-08-11 17:21:39 +0530

    second push

--- src/components/InputComp.vue
+++ src/components/InputComp.vue
@@ -1,33 +1,20 @@
  
 <template>
   <div>
-          <!-- <AddAnimal /> -->
     <v-app id="inspire">
       <v-main>
         <v-container>
           <AddAnimal></AddAnimal>
-    <!-- <v-btn class="float-right" depressed color="error" @click="addAnimal"> Create </v-btn> -->
-    
-       <p class="font-italic ma-10">
-    Animals of the kingdom
-    </p>
-          <v-row>
-            <v-col
-              v-for="(animal, index) in categories"
-              :key="index"
-              cols="4"
-            >
-              <v-card >
 
-                <v-img
-                 :src="animal.imgSrc"
-                 class="width:50px"
-                  
-                >
+          <p class="font-italic ma-10">Animals of the kingdom</p>
+          <v-row>
+            <v-col v-for="(animal, index) in categories" :key="index" cols="4">
+              <v-card>
+                <v-img :src="animal.imgSrc" class="width:50px">
                   <v-btn class="float-right" @click="deleteAnimal"> X </v-btn>
                 </v-img>
                 <v-card-title>
-            {{animal.name}}
+                  {{ animal.name }}
                 </v-card-title>
               </v-card>
             </v-col>
@@ -39,37 +26,29 @@
 </template>
 
 <script>
-import AddAnimal from './AddAnimal.vue'
+import AddAnimal from "./AddAnimal.vue";
 import { mapActions, mapState } from "vuex";
 export default {
   name: "inputComp",
   components: {
-
     AddAnimal,
   },
 
   computed: {
-    ...mapState('Anima', ["categories"]),
+    ...mapState("Anima", ["categories"]),
   },
 
-methods: {
-  ...mapActions('Anima', ['AddItems']),
-  ...mapActions('Anima', ['DeleteItems']),
+  methods: {
+    ...mapActions("Anima", ["AddItems"]),
+    ...mapActions("Anima", ["DeleteItems"]),
 
-  addAnimal() {
-    alert("Testing on the wAY")
-    this.AddItems ({
-    })
+    addAnimal() {
+      this.AddItems({});
+    },
+    deleteAnimal() {
+      alert("srgtrh");
+      this.DeleteItems({});
+    },
   },
-deleteAnimal() {
-  alert("srgtrh")
-  this.DeleteItems ({
-
-  })
-
-}
-  
-}
-
 };
 </script>
